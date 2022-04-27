fc9befda27592237728b1fbefd67373edd573e7d
Tymoteusz Karczewski <tymkar2020@gmail.com>
2022-04-27 20:46:00 +0200

    imports and basic structure

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,7 +1,18 @@
 import React from "react";
+import { urlFor, client } from "../../lib/client";
 
 const ProductDetails = () => {
-  return <div>ProductDetails</div>;
+  return (
+    <div>
+      <div className="product-detail-container">
+        <div>
+          <div className="image-container">
+            <img src="" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductDetails;
