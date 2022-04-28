480337851f81b92a93a5032fc814d3103b477177
Tymoteusz Karczewski <tymkar2020@gmail.com>
2022-04-28 11:14:38 +0200

    expandable cart

--- components/Cart.js
+++ components/Cart.js
@@ -13,11 +13,25 @@ import { useStateContext } from "../context/StateContext";
 import { urlFor } from "../lib/client";
 
 const Cart = () => {
-  const carfRef = useRef();
+  const cartRef = useRef();
   const { totalPrice, totalQuantities, cartItems, setShowCart } =
     useStateContext();
 
-  return <div>Cart</div>;
+  return (
+    <div className="cart-wrapper" ref={cartRef}>
+      <div className="cart-container">
+        <button
+          type="button"
+          className="cart-heading"
+          onClick={() => setShowCart(false)}
+        >
+          <AiOutlineLeft />
+          <span className="heading">Your Cart</span>
+          <span className="cart-num-items">({totalQuantities} items)</span>
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
