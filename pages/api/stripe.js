86bc45efb225366c0bc3bcc7c6bdb0ef9c4b10ae
Tymoteusz Karczewski <tymkar2020@gmail.com>
2022-04-28 17:31:42 +0200

    add new Shipping Rate

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -10,7 +10,10 @@ export default async function handler(req, res) {
         mode: "payment",
         payment_method_types: ["card"],
         billing_address_collection: "auto",
-        shipping_options: [{ shipping_rate: "shr_1KtXzaGid36GBUnjWm4BesvT" }],
+        shipping_options: [
+          { shipping_rate: "shr_1KtXzaGid36GBUnjWm4BesvT" },
+          { shipping_rate: "shr_1KtY1AGid36GBUnjbmctsgkG" },
+        ],
         line_items: req.body.map((item) => {
           const img = item.image[0].asset._ref;
           const newImage = img
