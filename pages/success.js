341a5ce022c5a1bc03b0c6144c8fbde9b600f626
Tymoteusz Karczewski <tymkar2020@gmail.com>
2022-04-28 18:03:14 +0200

    success page layout

--- pages/success.js
+++ pages/success.js
@@ -2,11 +2,34 @@ import React from "react";
 import { useState, useEffect } from "react/cjs/react.production.min";
 import Link from "next/link";
 import { BsBagCheckFill } from "react-icons/bs";
-import { useRouter } from "next/router";
 import { useStateContext } from "../context/StateContext";
 
 const Success = () => {
-  return <div>Success</div>;
+  const { setSartItems, setTotalPrice, setTotalQuantities } = useStateContext();
+
+  return (
+    <div className="success-wrapper">
+      <div className="success">
+        <p className="icon">
+          <BsBagCheckFill />
+        </p>
+        <h2>Thank you for your order!</h2>
+        <p className="email-msg">Check your email inbox for the receipt</p>
+        <p className="description">
+          If you have any question, please email{" "}
+          <a href="mailto: order@example.com" className="email">
+            order@example.com
+          </a>
+        </p>
+
+        <Link href="/">
+          <button type="button" width="300px" className="btn">
+            Continue Shopping
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Success;
