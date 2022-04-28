a6d8d6813c95fcbdf5057a91b3eb55762d8d55b7
Tymoteusz Karczewski <tymkar2020@gmail.com>
2022-04-28 18:08:04 +0200

    export setTotalPrice and setTotalQuantities

--- context/StateContext.js
+++ context/StateContext.js
@@ -107,6 +107,8 @@ export const StateContext = ({ children }) => {
         toggleCartItemQuantity,
         onRemove,
         setCartItems,
+        setTotalPrice,
+        setTotalQuantities,
       }}
     >
       {children}
