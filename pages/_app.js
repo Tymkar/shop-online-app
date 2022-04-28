7bf259d606137feee7ea1bbba1718daf700f7039
Tymoteusz Karczewski <tymkar2020@gmail.com>
2022-04-28 08:24:45 +0200

    import and add Toaster

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,12 @@ import React from "react";
 import "../styles/globals.css";
 import { Layout } from "../components";
 import { StateContext } from "../context/StateContext";
+import { Toaster } from "react-hot-toast";
 
 function MyApp({ Component, pageProps }) {
   return (
     <StateContext>
+      <Toaster />
       <Layout>
         <Component {...pageProps} />
       </Layout>
