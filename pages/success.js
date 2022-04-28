bc03e4b32e7d9456ce0d45d720952fb566de2b39
Tymoteusz Karczewski <tymkar2020@gmail.com>
2022-04-28 17:45:09 +0200

    succes page imports and basic component structure

---
+++ pages/success.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { useState, useEffect } from "react/cjs/react.production.min";
+import Link from "next/link";
+import { BsBagCheckFill } from "react-icons/bs";
+import { useRouter } from "next/router";
+import { useStateContext } from "../context/StateContext";
+
+const Success = () => {
+  return <div>Success</div>;
+};
+
+export default Success;
